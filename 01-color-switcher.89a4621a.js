17a406a8f3852ea3b83c93a3be85dc4022ef1640
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-06-02 20:10:50 +0000

    Deploying to gh-pages from @ Volodymyr-Bodak/goit-js-hw-09@fb3f7130f8554d0e229c978a8ca9aeb48edfa5e5 🚀

---
+++ 01-color-switcher.89a4621a.js
@@ -0,0 +1,2 @@
+let e;const t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0,t.addEventListener("click",(()=>{e=setInterval((()=>{document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0,d.disabled=!1}),1e3)})),d.addEventListener("click",(()=>{clearInterval(e),t.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.89a4621a.js.map
